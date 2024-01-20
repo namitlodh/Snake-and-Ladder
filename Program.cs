3194f761b948499cf3759924e4b68bcaf50b3fb4
unknown <namitlodh>
2024-01-20 13:41:04 +0530

    Use case 3: No play, Snake or ladder

--- Program.cs
+++ Program.cs
@@ -12,13 +12,37 @@ namespace Snake_and_Ladder
         {
             int playerpositoon = 0;
             int diceroll = Diceroll();
-            Console.WriteLine(diceroll);
+
+            Play();
+            Console.ReadLine();
+        }
+        static void Play()
+        { 
+            Random rand = new Random();
+            int options= rand.Next(0,3);
+            switch(options)
+            {
+                case 0:
+                    Console.WriteLine("No move");
+                    break;
+                case 1:
+                    Console.WriteLine($"Its a Ladder, Move forward from {Position()} to {Position() + Diceroll()}");
+                    break;
+                case 2:
+                    Console.WriteLine($"Its a Snake, Move forward from {Position()} to {Position() - Diceroll()}");
+                    break;
+            }
             Console.ReadLine();
         }
         static int Diceroll()
         {
             Random rand = new Random();
             return rand.Next(1, 7);
+        }
+        static int Position()
+        {
+            Random rand = new Random();
+            return rand.Next(1, 101);
         }
     }
 }
