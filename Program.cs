035cdfcc52c5453b2c027a63c55dbf17639b695f
unknown <namitlodh>
2024-01-22 10:53:53 +0530

    Use case 6: Dice count

--- Program.cs
+++ Program.cs
@@ -16,24 +16,31 @@ namespace Snake_and_Ladder
             {
                 Console.WriteLine($"Dice count: {count++}");
                 Console.WriteLine($"Player is currently at position {playposition}. Press Enter to simulate a player's turn.");
+                int x = Diceroll();
+                Console.WriteLine($"Dice gave: {x}");
                 Console.ReadLine();
                 Random rand = new Random();
                 int options = rand.Next(0, 3);
                 switch (options)
                 {
                     case 0:
-                        Console.WriteLine("Its a Ladder, Move forward");
-                        playposition += Diceroll();
-                        Console.WriteLine($"Player moved from {playposition - Diceroll()} to {playposition}");
+                        Console.WriteLine("Player found a ladder! Move forward.");
+                        playposition += x;
+                        int a = rand.Next(playposition, 101);
+                        Console.WriteLine($"Player moved from {playposition} to {a}");
+                        playposition = a;
                         break;
                     case 1:
                         Console.WriteLine("Its a Snake, Move backward");
-                        int previousPosition = playposition;
-                        playposition -= Diceroll();
-                        Console.WriteLine($"Player moved from {previousPosition} to {playposition}.");
+                        playposition+=x;
+                        int b = rand.Next(2,playposition);
+                        Console.WriteLine($"Player moved from {playposition} to {b}.");
+                        playposition = b;
                         break;
                     case 2:
-                        Console.WriteLine("No move");
+                        int previous = playposition;
+                        playposition += x;
+                        Console.WriteLine($"Player moved from {previous} to {playposition}.");
                         break;
                 }
 
@@ -44,14 +51,17 @@ namespace Snake_and_Ladder
                 }
                 else if (playposition == 100)
                 {
+                    Console.WriteLine("Total die count " + count);
                     Console.WriteLine("Player reached the winning position (100)!");
                 }
                 else if (playposition > 100)
                 {
                     Console.WriteLine($"Player is in the same position {playposition-Diceroll()}!");
                 }
+            Console.ReadLine();
             }
         }
+
         static int Diceroll()
         {
             Random rand = new Random();
